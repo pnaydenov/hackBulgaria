2a942b3c7eb4883d9855def5e1b5756229079001
pnaydenov <pln@abv.bg>
2014-05-04 23:09:22 +0300

    Update Pr33.java

--- Pr33.java
+++ Pr33.java
@@ -47,19 +47,32 @@ public class Pr33 {
 
 		
 			int[][] m1 = new int[][]{
-					{4,9,2},
-					{3,5,7},
-					{8,1,6}
+					{1, 2, 3},
+					{4, 5, 6},
+					{7, 8, 9}
 			};
 			int[][] m2 = new int[][]{
-					{4,9,2},
-					{3,5,7},
-					{8,1,6}
+					{4, 9, 2},
+					{3, 5, 7},
+					{8, 1, 6}
 			};
 			int[][] m3 = new int[][]{
-					{4,9,2},
-					{3,5,7},
-					{8,1,6}
+					{7, 12, 1, 14},
+					{2, 13, 8, 11},
+					{16, 3, 10, 5},
+					{9, 6, 15, 4}
+			};
+			
+			int[][] m4 = new int[][]{
+					{23, 28, 21},
+					{22, 24, 26},
+					{27, 20, 25}
+			};
+			
+			int[][] m5 = new int[][]{
+					{16, 23, 17},
+					{78, 32, 21},
+					{17, 16, 15}
 			};
 			
 			
@@ -69,5 +82,9 @@ public class Pr33 {
 			System.out.println(msc.magic_square(m1));
 			System.out.println(msc.magic_square(m2));
 			System.out.println(msc.magic_square(m3));
+			System.out.println(msc.magic_square(m4));
+			System.out.println(msc.magic_square(m5));
+
 	}
+	
 }
